94fe81a0998acaf9f97a8149e079161e188795eb
Augustin <a.reille@net.estia.fr>
2017-11-17 16:50:24 +0000

    first commit

---
+++ helloworld.c
@@ -0,0 +1,15 @@
+#include <mpi.h>
+#include <stdio.h>
+
+main(int argc, char *argv[])
+{
+    int npes, myrank;
+    MPI_Status status;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &npes);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+
+    printf("From process %d out of %d, Hello world!\n", myrank, npes);
+
+    MPI_Finalize();
+}
